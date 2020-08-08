cfafa6d3b341ed65eab20d3c6a6d5957821f349b
LazarJovic <lazar.13jovic@gmail.com>
2020-08-08 15:11:10 +0200

    Create register property page
    Created a page for property registration.

--- Frontend/property-renting-app/src/app/features/landlord/register-property/register-property.component.ts
+++ Frontend/property-renting-app/src/app/features/landlord/register-property/register-property.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPropertyComponent implements OnInit {
 
+  imageObject: any = null;
+  vehicleImage: File;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onFileSelected(event) {
+    if (event.target.files.length > 1) {
+      // this.toast.info("You can upload maximum 1 image for vehicle.");
+      return;
+    }
+    for (const file of event.target.files) {
+      this.vehicleImage = file;
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.imageObject = reader.result;
+      };
+    }
   }
 
 }
