5c285de3afd60c546a040ae290a892d30bf8de7d
LazarJovic <lazar.13jovic@gmail.com>
2020-08-31 16:11:43 +0200

    Move tables to one component

--- Frontend/property-renting-app/src/app/features/landlord/landlord-booking-requests/landlord-booking-requests.component.ts
+++ Frontend/property-renting-app/src/app/features/landlord/landlord-booking-requests/landlord-booking-requests.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandlordBookingRequestsComponent implements OnInit {
 
+  displayedColumnsPending: string[] = ['location', 'pendingDateTime', 'bookingStart', 'bookingEnd', 'clientEmail', 'btnDetails',
+                                      'btnAccept', 'btnDeny'];
+  displayedColumnsPaid: string[] = ['location', 'bookingStart', 'bookingEnd', 'pricePerNight', 'clientEmail', 'btnDetails', 'btnFinish'];
+  displayedColumnsReserved: string[] = ['location', 'acceptanceDateTime', 'bookingStart', 'bookingEnd', 'pricePerNight', 'securityDeposit',
+                                        'clientEmail', 'btnDetails', 'btnMessages', 'btnCancel'];
+  displayedColumnsFinished: string[] = ['location', 'bookingStart', 'bookingEnd', 'clientEmail', 'btnDetails'];
+  displayedColumnsCanceled: string[] = ['location', 'bookingStart', 'bookingEnd', 'clientEmail', 'btnDetails'];
+
+  isLandlord: boolean;
   constructor() { }
 
   ngOnInit() {
   }
+
+  requestDetails(request) {}
+
+  accept(request) {}
+
+  deny(request) {}
+
+  messages(request) {}
+
+  pay(request) {}
+
+  cancel(request) {}
+
+  finish(request) {}
 
 }
